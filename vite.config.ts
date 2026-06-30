606b59e8994a5dc854d02912fe49c234b327ea5a
jowa131 <269414762+jowa131@users.noreply.github.com>
2026-06-30 20:33:19 +0900

    chore(deps): update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [wasm(), topLevelAwait()],
   worker: {
     plugins: () => [wasm(), topLevelAwait()],
